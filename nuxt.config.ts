396471db75fe7c153a2cceeb921f1eba44d44ae3
LouisKo <louiskoxiang@gmail.com>
2023-02-17 15:45:16 +0800

    [F2E] Set App Head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      title: 'Image-Generation',
+      meta: [
+        { name: 'description', content: 'Nuxt3 ChatGpt OpenAI Image Generation' }
+      ]
+    }
+  },
   typescript: {
     // typeCheck: true
   },
