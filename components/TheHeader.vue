8ddd284814ed4f30fa3553e09536bf7bea5ff44f
LouisKo <louiskoxiang@gmail.com>
2023-02-20 16:31:41 +0800

    [F2E] Adjust Class

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -15,11 +15,8 @@
         </select>
       </p>
       <button
-        data-collapse-toggle="navbar-default"
         type="button"
         class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="navbar-default"
-        aria-expanded="false"
         @click="openDrawer"
       >
         <span class="sr-only">Open main menu</span>
